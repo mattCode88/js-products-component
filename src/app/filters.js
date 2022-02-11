b4c6d1760c63f922986a38cbe638b3f086f57c2d
mattcode88 <mattcode88@gmail.com>
2022-02-11 15:00:04 +0100

    inizi progetto

---
+++ src/app/filters.js
@@ -0,0 +1,22 @@
+// import MainProducts from ".index.js"
+
+// class Filters extends HTMLElement{
+
+//     constructor() {
+//         super()
+
+//         this.ciao()
+//     }
+// }
+
+// ciao = () => {
+//     console.log('ciao')
+// }
+
+// export default Filters;
+
+function ciao() {
+    console.log('ciao')
+}
+
+export default ciao;
